874edea8074d4d542168eee1376cd4be68f534c7
Hernan Badenes <herchu@inorbit.ai>
2026-02-09 14:14:27 -0300

    moved to client folder

--- web/app/client/main.jsx
+++ web/app/client/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
-import App from "/imports/ui/App";
-import "/imports/ui/styles.css";
+import App from "/imports/client/App";
+import "/imports/client/styles.css";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
